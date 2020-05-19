fd54c18d4fc9c404b50133f4259e17084611b272
James Brotsos <j.brotsos@gmail.com>
2020-05-19 15:35:00 +0000

    Update AddNewUser.aspx.cs

--- WebGoat/AddNewUser.aspx.cs
+++ WebGoat/AddNewUser.aspx.cs
@@ -10,6 +10,8 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 
+// test
+
 namespace OWASP.WebGoat.NET
 {
 	public partial class AddNewUser : System.Web.UI.Page
